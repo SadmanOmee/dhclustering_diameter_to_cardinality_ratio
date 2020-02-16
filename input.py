d02f83f9e9283fcc0cae8fbe6db3cd08f7cf8e3a
SadmanSupernooba <omee.sadman@gmail.com>
2020-02-16 17:11:55 +0600

    simultaneous 2 distributions code updated

--- input.py
+++ input.py
@@ -6,37 +6,41 @@ from sklearn.cluster import KMeans
 xCoordinates = []
 yCoordinates = []
 #points = np.random.weibull(2.0, size=[100, 2])
-#points = np.random.normal(0.0, 1.0, size=[200, 2]) #gaussian distribution
+#points2 = np.random.normal(0.0, 2.0, size=[200, 2]) #gaussian distribution
 #points = np.random.exponential(1.0, size=[500, 2])
 #points = np.random.beta(1.0, 1.0, size=[400, 2])
 #points = np.random.binomial(size=[100, 2], n=5, p= 0.5)
 #points = np.random.binomial(size=[100, 2], n=1, p= 0.5) #bernoulli distribution
-#points2 = np.random.gamma(2.0, 2.0, size=[400, 2])
+#points = np.random.gamma(2.0, 2.0, size=[400, 2])
 #points = np.random.pareto(3.0, size=[800, 2])
 #points = np.random.poisson(5, size=[50, 2])
 #points = np.random.lognormal(3, 1, size=[700, 2])
 #points = np.random.geometric(p=0.35, size=[100, 2])
-points = np.random.rayleigh(3, size=[400, 2])
+points = np.random.rayleigh(3, size=[200, 2])
 #points, y = make_blobs(n_samples=200, centers=12, cluster_std=0.60, random_state=8)
 
 inp = str(len(points)) + "\n"
+#inp = str(len(points) + len(points2)) + "\n"
 
 file = open("input1.txt","w")
 file.write(inp)
 
 for i in range (0, len(points)):
-    #np.around(points[i], decimals=100)
     xCoordinates.append(points[i][0])
     yCoordinates.append(points[i][1])
     inp = str(points[i][0]) + " " + str(points[i][1]) + "\n"
     file.write(inp)
     
-    #xCoordinates.append(points2[i][0] + 9)
-    #yCoordinates.append(points2[i][1] + 9)
-    #inp = str(points2[i][0] + 9) + " " + str(points2[i][1] + 9) + "\n"
-    #file.write(inp)
+    #None
+'''   
+for i in range (0, len(points2)):
+    xCoordinates.append(points2[i][0] + 10)
+    yCoordinates.append(points2[i][1] + 10)
+    inp = str(points2[i][0] + 10) + " " + str(points2[i][1] + 10) + "\n"
+    file.write(inp)
     
     #None
+'''
 print(points)
 #print(points[0][1])
 
@@ -49,7 +53,7 @@ file.close()
 
 plt.scatter(points[:, 0], points[:, 1], color="red")
 #plt.scatter(points2[:, 0], points2[:, 1], color="blue")
-plt.scatter(xCoordinates, yCoordinates, color="blue")
+plt.scatter(xCoordinates, yCoordinates, color="black")
 
 plt.show()
 
