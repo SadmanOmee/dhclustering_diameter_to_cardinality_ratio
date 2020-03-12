df4024e12490ec547b37184b7facd4c4bcb5b368
SadmanSupernooba <omee.sadman@gmail.com>
2020-03-12 18:04:25 +0600

    paper stuff added

--- clusteringNewObjFunc.cpp
+++ clusteringNewObjFunc.cpp
@@ -89,7 +89,7 @@ double mahalanobisDistance(point a, point b)
 
 double pointDistance(point a, point b)
 {
-    return chebychevDistance(a, b);
+    return manhattanDistance(a, b);
 }
 /** distance measure functions implementation end*/
 
