b5097294458ff8bbfbb768e68be67ec9ae36afb5
SadmanSupernooba <omee.sadman@gmail.com>
2021-01-08 20:20:27 +0600

    more cluster quality metrics added

--- other_clustering_algorithms.py
+++ other_clustering_algorithms.py
@@ -4,12 +4,14 @@ from sklearn.datasets.samples_generator import make_blobs, make_moons, make_circ
 from sklearn.cluster import KMeans, AffinityPropagation, SpectralClustering, \
     AgglomerativeClustering, DBSCAN, OPTICS, Birch, MeanShift
 from sklearn.mixture import GaussianMixture
-from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn.metrics.cluster import adjusted_rand_score, adjusted_mutual_info_score, homogeneity_score, \
+     completeness_score, v_measure_score, fowlkes_mallows_score, silhouette_score, calinski_harabasz_score, \
+     davies_bouldin_score
     
 k = 2
 
 inputFile = open("input1.txt","r")
-n = int(inputFile.readline())
+n = int(inputFile.readline());
 points = []
 qMetrics = True
 trueLabels = []
@@ -57,7 +59,16 @@ def affinityPropagation():
     plt.title('Affinity Propagation')
     plt.show()
     if qMetrics == True:
+        #print("Affinity Propagation RI:", rand_score(trueLabels, labels))
         print("Affinity Propagation ARI:", adjusted_rand_score(trueLabels, labels))
+        print("Affinity Propagation AMI:", adjusted_mutual_info_score(trueLabels, labels))
+        print("Affinity Propagation HS:", homogeneity_score(trueLabels, labels))
+        print("Affinity Propagation CS:", completeness_score(trueLabels, labels))
+        print("Affinity Propagation VM:", v_measure_score(trueLabels, labels))
+        print("Affinity Propagation FM:", fowlkes_mallows_score(trueLabels, labels))
+        print("Affinity Propagation SC:", silhouette_score(points, labels, metric='euclidean'))
+        print("Affinity Propagation CH:", calinski_harabasz_score(points, labels))
+        print("Affinity Propagation DB:", davies_bouldin_score(points, labels))
 
 
 '''---------kmeans----------'''
@@ -74,6 +85,14 @@ def kmeans():
     plt.show()
     if qMetrics == True:
         print("kmeans ARI:", adjusted_rand_score(trueLabels, labels))
+        print("kmeans AMI:", adjusted_mutual_info_score(trueLabels, labels))
+        print("kmeans HS:", homogeneity_score(trueLabels, labels))
+        print("kmeans CS:", completeness_score(trueLabels, labels))
+        print("kmeans VM:", v_measure_score(trueLabels, labels))
+        print("kmeans FM:", fowlkes_mallows_score(trueLabels, labels))
+        print("kmeans SC:", silhouette_score(points, labels, metric='euclidean'))
+        print("kmeans CH:", calinski_harabasz_score(points, labels))
+        print("kmeans DB:", davies_bouldin_score(points, labels))
 
 
 '''--------Spectral Clustering---------'''
@@ -90,6 +109,14 @@ def spectralClustering():
     plt.show()
     if qMetrics == True:
         print("Spectral Clustering ARI:", adjusted_rand_score(trueLabels, labels))
+        print("Spectral Clustering AMI:", adjusted_mutual_info_score(trueLabels, labels))
+        print("Spectral Clustering HS:", homogeneity_score(trueLabels, labels))
+        print("Spectral Clustering CS:", completeness_score(trueLabels, labels))
+        print("Spectral Clustering VM:", v_measure_score(trueLabels, labels))
+        print("Spectral Clustering FM:", fowlkes_mallows_score(trueLabels, labels))
+        print("Spectral Clustering SC:", silhouette_score(points, labels, metric='euclidean'))
+        print("Spectral Clustering CH:", calinski_harabasz_score(points, labels))
+        print("Spectral Clustering DB:", davies_bouldin_score(points, labels))
 
 '''--------Agglomerative Clustering---------'''
 def wardAC():
@@ -105,6 +132,14 @@ def wardAC():
     plt.show()
     if qMetrics == True:
         print("Agglomerative Clustering ARI:", adjusted_rand_score(trueLabels, labels))
+        print("Agglomerative Clustering AMI:", adjusted_mutual_info_score(trueLabels, labels))
+        print("Agglomerative Clustering HS:", homogeneity_score(trueLabels, labels))
+        print("Agglomerative Clustering CS:", completeness_score(trueLabels, labels))
+        print("Agglomerative Clustering VM:", v_measure_score(trueLabels, labels))
+        print("Agglomerative Clustering FM:", fowlkes_mallows_score(trueLabels, labels))
+        print("Agglomerative Clustering SC:", silhouette_score(points, labels, metric='euclidean'))
+        print("Agglomerative Clustering CH:", calinski_harabasz_score(points, labels))
+        print("Agglomerative Clustering DB:", davies_bouldin_score(points, labels))
 
 
 '''--------OPTICS---------'''
@@ -134,6 +169,14 @@ def birch():
     plt.show()
     if qMetrics == True:
         print("Birch ARI:", adjusted_rand_score(trueLabels, labels))
+        print("Birch AMI:", adjusted_mutual_info_score(trueLabels, labels))
+        print("Birch HS:", homogeneity_score(trueLabels, labels))
+        print("Birch CS:", completeness_score(trueLabels, labels))
+        print("Birch VM:", v_measure_score(trueLabels, labels))
+        print("Birch FM:", fowlkes_mallows_score(trueLabels, labels))
+        print("Birch SC:", silhouette_score(points, labels, metric='euclidean'))
+        print("Birch CH:", calinski_harabasz_score(points, labels))
+        print("Birch DB:", davies_bouldin_score(points, labels))
 
 
 '''--------MeanShift---------'''
@@ -151,6 +194,14 @@ def meanShift():
     plt.show()
     if qMetrics == True:
         print("MeanShift ARI:", adjusted_rand_score(trueLabels, labels))
+        print("MeanShift AMI:", adjusted_mutual_info_score(trueLabels, labels))
+        print("MeanShift HS:", homogeneity_score(trueLabels, labels))
+        print("MeanShift CS:", completeness_score(trueLabels, labels))
+        print("MeanShift VM:", v_measure_score(trueLabels, labels))
+        print("MeanShift FM:", fowlkes_mallows_score(trueLabels, labels))
+        print("MeanShift SC:", silhouette_score(points, labels, metric='euclidean'))
+        print("MeanShift CH:", calinski_harabasz_score(points, labels))
+        print("MeanShift DB:", davies_bouldin_score(points, labels))
 
 
 '''--------Gaussian Mixture---------'''
@@ -164,10 +215,18 @@ def gaussianMixture():
     DataY = [points[i][1] for i in range (len(points))]
     plt.scatter(DataX, DataY, c = labels, cmap='Set1')
     #plt.scatter(C[:, 0], C[:, 1], marker='*', c='#050505')
-    plt.title('GaussianMixture')
+    plt.title('Gaussian Mixture')
     plt.show()
     if qMetrics == True:
         print("Gaussian Mixture ARI:", adjusted_rand_score(trueLabels, labels))
+        print("Gaussian Mixture AMI:", adjusted_mutual_info_score(trueLabels, labels))
+        print("Gaussian Mixture HS:", homogeneity_score(trueLabels, labels))
+        print("Gaussian Mixture CS:", completeness_score(trueLabels, labels))
+        print("Gaussian Mixture VM:", v_measure_score(trueLabels, labels))
+        print("Gaussian Mixture FM:", fowlkes_mallows_score(trueLabels, labels))
+        print("Gaussian Mixture SC:", silhouette_score(points, labels, metric='euclidean'))
+        print("Gaussian Mixture CH:", calinski_harabasz_score(points, labels))
+        print("Gaussian Mixture DB:", davies_bouldin_score(points, labels))
 
 
 
@@ -185,6 +244,14 @@ def dbscan():
     plt.show()
     if qMetrics == True:
         print("DBSCAN ARI:", adjusted_rand_score(trueLabels, labels))
+        print("DBSCAN AMI:", adjusted_mutual_info_score(trueLabels, labels))
+        print("DBSCAN HS:", homogeneity_score(trueLabels, labels))
+        print("DBSCAN CS:", completeness_score(trueLabels, labels))
+        print("DBSCAN VM:", v_measure_score(trueLabels, labels))
+        print("DBSCAN FM:", fowlkes_mallows_score(trueLabels, labels))
+        #print("DBSCAN SC:", silhouette_score(points, labels, metric='euclidean'))
+        #print("DBSCAN CH:", calinski_harabasz_score(points, labels))
+        #print("DBSCAN DB:", davies_bouldin_score(points, labels))
 
 
 
